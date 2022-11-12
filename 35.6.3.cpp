867eae6ff69296cfc4408b7c874ef89cdbcbae78
lebed <lebed@DESKTOP-BKIICKL>
2022-11-12 09:38:51 +0300

    Add project files.

---
+++ 35.6.3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+
+
+
+
+
+int main()
+{
+    std::cout << "Enter path: ";
+    std::filesystem::path path;
+    std::cin >> path;
+    std::cout << "Enter extension: ";
+    std::string ext;
+    std::cin >> ext;
+    if (ext[0] != '.')
+    {
+        ext.insert(ext.begin(), '.');
+    }
+        
+    auto recursiveGetFileNamesByExtension = [](std::filesystem::path file, const std::string& extension)
+    {
+        const int compareEqual = 0;
+        if (std::filesystem::is_regular_file(file) && (file.extension().compare(extension) == compareEqual))
+        {
+            std::cout << file << std::endl;
+        }       
+    };
+
+    for (const auto& p : std::filesystem::recursive_directory_iterator(path))
+    {
+        recursiveGetFileNamesByExtension(p.path(), ext);
+    }
+}
